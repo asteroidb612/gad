a3fd21564325acfa18beb08208bdca3a3f9edc59
Mike Kissinger <mis4mike@gmail.com>
2020-01-11 20:46:14 -0500

    Updates

---
+++ precache-manifest.6614921918870f07e510f8db4ff1464d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e8f689851d039f1736b84b527363f77",
+    "url": "/gad-client/static/media/gad-title.9e8f6898.png"
+  },
+  {
+    "revision": "86ba05329752c1ad51e23164e9f15fe9",
+    "url": "/gad-client/static/media/gad-logo-small-white.86ba0532.png"
+  },
+  {
+    "revision": "13dc531a2ffd9ca7eeb500515774fa74",
+    "url": "/gad-client/static/media/GlacialIndifference-Regular.13dc531a.otf"
+  },
+  {
+    "revision": "eadb7501547b6ede436678d2048754d4",
+    "url": "/gad-client/static/media/CheddarGothic-Sans.eadb7501.otf"
+  },
+  {
+    "revision": "6f68a66bd0bc9d311d77",
+    "url": "/gad-client/static/js/runtime~main.8b9589ed.js"
+  },
+  {
+    "revision": "8d2b8eb6708ad2bc40ef",
+    "url": "/gad-client/static/js/main.991254f2.chunk.js"
+  },
+  {
+    "revision": "91f581c9883034c6b6d9",
+    "url": "/gad-client/static/js/2.0ac367af.chunk.js"
+  },
+  {
+    "revision": "8d2b8eb6708ad2bc40ef",
+    "url": "/gad-client/static/css/main.e95c54a1.chunk.css"
+  },
+  {
+    "revision": "441e3c51b34eb2871c6e377b6ef63053",
+    "url": "/gad-client/index.html"
+  }
+];
